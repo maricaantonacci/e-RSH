ab76b4ab541d15f18f3ee61df546acc6f70003c8
maricaantonacci <marica.antonacci@gmail.com>
2016-07-01 11:11:25 +0000

    Templates renamed

--- scripts/gen_chronos_job_request.sh
+++ scripts/gen_chronos_job_request.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "Usage: ./`basename $0` <TOSCA J2 TEMPLATE> <INPUTs JSON FILE>"
-	exit 1
-fi
-
-TOSCA_J2TEMPLATE=$1
-INPUTS_J2TEMPLATE=$(basename $TOSCA_J2TEMPLATE .j2)_inputs.j2
-REQUEST_FILE=$(basename $TOSCA_J2TEMPLATE .j2)_request.j2
+TOSCA_J2TEMPLATE=tosca_template.j2
+INPUTS_J2TEMPLATE=tosca_template_inputs.j2
+REQUEST_FILE=job_request.j2
+PARAMETERS_FILE=input_parameters.json
 
 cat <<EOF > $REQUEST_FILE
 {
@@ -19,6 +15,6 @@ $(cat $INPUTS_J2TEMPLATE)
 EOF
 
 BIN_DIR=$(dirname $0)
-python "$BIN_DIR"/jinja2template2json.py --template $REQUEST_FILE --parameters $2
+python "$BIN_DIR"/jinja2template2json.py --template $REQUEST_FILE --parameters $PARAMETERS_FILE
 
 rm $REQUEST_FILE
