14a9056912904c58f6f23a8366a89e71bc1c70b9
maricaantonacci <marica.antonacci@gmail.com>
2016-07-02 23:59:41 +0000

    Added script for Lifewatch

---
+++ scripts/Lifewatch/run_lifewatch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mkdir -p /onedata/input
+mkdir -p /onedata/output
+
+ONECLIENT_AUTHORIZATION_TOKEN="$INPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$INPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o ro /onedata/input || exit 1
+ONECLIENT_AUTHORIZATION_TOKEN="$OUTPUT_ONEDATA_TOKEN" PROVIDER_HOSTNAME="$OUTPUT_ONEDATA_PROVIDERS" oneclient --no_check_certificate --authentication token -o rw /onedata/output || exit 1
+
+echo Start at $(date)
+
+INPUTDIR="/onedata/input/$INPUT_ONEDATA_SPACE/$INPUT_PATH"
+OUTPUTDIR="/onedata/output/$OUTPUT_ONEDATA_SPACE/$OUTPUT_PATH"
+
+
+WORKDIR=$MESOS_SANDBOX
+#WORKDIR="$OUTPUTDIR"
+
+# Extract input
+echo Extracting input
+tar xvfz "$INPUTDIR/delft3d_repository.tar.gz" --no-same-owner -C "$WORKDIR" || exit 1
+cd "$WORKDIR"/delft3d_repository/06_delwaq || exit 2
+
+echo Run test
+# Run Rscript
+./run_delwaq.sh || exit 1
+tar cvfz model0.tgz *
+
+# Collect output
+mkdir -p "$OUTPUTDIR"/
+cp $(echo "$OUTPUT_FILENAMES" | tr ',' ' ' ) "$OUTPUTDIR"/  || exit 1
+
+echo End at $(date)
+
+sleep 5
+
+umount /onedata/input || exit 1
+umount /onedata/output || exit 1
